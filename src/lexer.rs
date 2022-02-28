bdfdb38af4084a3e59527a935a9854b2d0f47c6f
Louis DEVIE <louisdevie.contact@gmail.com>
2022-02-28 18:24:58 +0100

    refactored the lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -5,35 +5,64 @@ use regex::Regex;
 pub enum Token {
     BlockBegin {},
     BlockEnd {},
+    DoubleQuotes {},
     Identifier { name: String },
     Keyword { keyword: Keyword },
-    LeftOperator { operator: LeftOperator },
+    UnaryLeftOperator { operator: UnaryLeftOperator },
     LineEnd {},
-    StringLiteral { value: String },
+    StringContent { value: String },
     Variable { name: String },
 }
 
+#[derive(Debug)]
+pub enum Keyword {
+    META,
+    NAME,
+    ARGS,
+    PARAM,
+    OPTIONAL,
+    DESCRIPTION,
+    RUN,
+    IF,
+    THEN,
+    PRINTLN,
+    ELSE,
+    SUCCESS,
+}
+
+#[derive(Debug)]
+pub enum UnaryLeftOperator {
+    NOTNULL,
+}
+
 pub fn parse(script: &str) -> Vec<Token> {
+    let mut result = Vec::new();
+
+    parse_generic(script, &mut result, 0);
+
+    println!("{:#?}", result);
+    return result;
+}
+
+fn parse_generic(input: &str, output: &mut Vec<Token>, mut cursor: usize) -> usize {
     lazy_static! {
         static ref KEYWORD: Regex = Regex::new(r"\b(args|(as|(authors|(break|(case|(close|(default|(description|(do|(else|(fail|(flag|(for(each)?|(from|(func|(homepage|(if|(in|(let|(long|(meta|(mode|(name|(open|(option(al)?|(param|(print(ln)?|(read(ln)?|(repeat|(return|(run|(short|(skip|(success|(switch|(then|(to|(type|(until|(version|(while)))))))))))))))))))))))))))))))))))))))))\b").unwrap();
         static ref SPACING: Regex = Regex::new(r"\s+").unwrap();
         static ref OBRACE: Regex = Regex::new(r"\{").unwrap();
         static ref CBRACE: Regex = Regex::new(r"\}").unwrap();
         static ref SEMICOLON: Regex = Regex::new(r";").unwrap();
-        static ref STRING: Regex = Regex::new("\".*?[^\\\\]?\"").unwrap();
+        static ref DQUOTES: Regex = Regex::new("\"").unwrap();
         static ref IDENTIFIER: Regex = Regex::new(r"\b\w+\b").unwrap();
         static ref VARIABLE: Regex = Regex::new(r"\$\b\w+\b").unwrap();
         static ref LOPERATOR: Regex = Regex::new(r"\?").unwrap();
     }
 
-    let mut tokens = Vec::new();
+    let end = input.len();
 
-    let end = script.len();
-    let mut cursor = 0;
     while cursor < end {
-        if let Some(m) = KEYWORD.find_at(script, cursor) {
+        if let Some(m) = KEYWORD.find_at(input, cursor) {
             if m.start() == cursor {
-                tokens.push(Token::Keyword {
+                output.push(Token::Keyword {
                     keyword: match m.as_str() {
                         "meta" => Keyword::META,
                         "name" => Keyword::NAME,
@@ -55,22 +84,17 @@ pub fn parse(script: &str) -> Vec<Token> {
             }
         }
 
-        if let Some(m) = STRING.find_at(script, cursor) {
+        if let Some(m) = DQUOTES.find_at(input, cursor) {
             if m.start() == cursor {
-                let mut without_quotes = String::from(m.as_str());
-                without_quotes.remove(0);
-                without_quotes.pop().unwrap();
-                tokens.push(Token::StringLiteral {
-                    value: without_quotes,
-                });
-                cursor = m.end();
+                output.push(Token::DoubleQuotes {});
+                cursor = parse_double_quoted(input, output, cursor);
                 continue;
             }
         }
 
-        if let Some(m) = IDENTIFIER.find_at(script, cursor) {
+        if let Some(m) = IDENTIFIER.find_at(input, cursor) {
             if m.start() == cursor {
-                tokens.push(Token::Identifier {
+                output.push(Token::Identifier {
                     name: String::from(m.as_str()),
                 });
                 cursor = m.end();
@@ -78,11 +102,11 @@ pub fn parse(script: &str) -> Vec<Token> {
             }
         }
 
-        if let Some(m) = VARIABLE.find_at(script, cursor) {
+        if let Some(m) = VARIABLE.find_at(input, cursor) {
             if m.start() == cursor {
                 let mut without_dollar = String::from(m.as_str());
                 without_dollar.remove(0);
-                tokens.push(Token::Variable {
+                output.push(Token::Variable {
                     name: without_dollar,
                 });
                 cursor = m.end();
@@ -90,41 +114,41 @@ pub fn parse(script: &str) -> Vec<Token> {
             }
         }
 
-        if let Some(m) = OBRACE.find_at(script, cursor) {
+        if let Some(m) = OBRACE.find_at(input, cursor) {
             if m.start() == cursor {
-                tokens.push(Token::BlockBegin {});
+                output.push(Token::BlockBegin {});
                 cursor = m.end();
                 continue;
             }
         }
 
-        if let Some(m) = CBRACE.find_at(script, cursor) {
+        if let Some(m) = CBRACE.find_at(input, cursor) {
             if m.start() == cursor {
-                tokens.push(Token::BlockEnd {});
+                output.push(Token::BlockEnd {});
                 cursor = m.end();
                 continue;
             }
         }
 
-        if let Some(m) = SEMICOLON.find_at(script, cursor) {
+        if let Some(m) = SEMICOLON.find_at(input, cursor) {
             if m.start() == cursor {
-                tokens.push(Token::LineEnd {});
+                output.push(Token::LineEnd {});
                 cursor = m.end();
                 continue;
             }
         }
 
-        if let Some(m) = LOPERATOR.find_at(script, cursor) {
+        if let Some(m) = LOPERATOR.find_at(input, cursor) {
             if m.start() == cursor {
-                tokens.push(Token::LeftOperator {
-                    operator: LeftOperator::NOTNULL,
+                output.push(Token::UnaryLeftOperator {
+                    operator: UnaryLeftOperator::NOTNULL,
                 });
                 cursor = m.end();
                 continue;
             }
         }
 
-        if let Some(m) = SPACING.find_at(script, cursor) {
+        if let Some(m) = SPACING.find_at(input, cursor) {
             if m.start() == cursor {
                 cursor = m.end();
                 continue;
@@ -134,27 +158,90 @@ pub fn parse(script: &str) -> Vec<Token> {
         panic!("Invalid character at position {}", cursor);
     }
 
-    println!("{:#?}", tokens);
-    return tokens;
+    return cursor;
 }
 
-#[derive(Debug)]
-pub enum Keyword {
-    META,
-    NAME,
-    ARGS,
-    PARAM,
-    OPTIONAL,
-    DESCRIPTION,
-    RUN,
-    IF,
-    THEN,
-    PRINTLN,
-    ELSE,
-    SUCCESS,
-}
+fn parse_double_quoted(input: &str, output: &mut Vec<Token>, mut cursor: usize) -> usize {
+    lazy_static! {
+        static ref LOPERATOR: Regex = Regex::new(r"\?").unwrap();
+    }
 
-#[derive(Debug)]
-pub enum LeftOperator {
-    NOTNULL,
+    let end = input.len();
+
+    while cursor < end {
+        if let Some(m) = DQUOTES.find_at(input, cursor) {
+            if m.start() == cursor {
+                output.push(Token::DoubleQuotes {});
+
+                continue;
+            }
+        }
+
+        if let Some(m) = IDENTIFIER.find_at(input, cursor) {
+            if m.start() == cursor {
+                output.push(Token::Identifier {
+                    name: String::from(m.as_str()),
+                });
+                cursor = m.end();
+                continue;
+            }
+        }
+
+        if let Some(m) = VARIABLE.find_at(input, cursor) {
+            if m.start() == cursor {
+                let mut without_dollar = String::from(m.as_str());
+                without_dollar.remove(0);
+                output.push(Token::Variable {
+                    name: without_dollar,
+                });
+                cursor = m.end();
+                continue;
+            }
+        }
+
+        if let Some(m) = OBRACE.find_at(input, cursor) {
+            if m.start() == cursor {
+                output.push(Token::BlockBegin {});
+                cursor = m.end();
+                continue;
+            }
+        }
+
+        if let Some(m) = CBRACE.find_at(input, cursor) {
+            if m.start() == cursor {
+                output.push(Token::BlockEnd {});
+                cursor = m.end();
+                continue;
+            }
+        }
+
+        if let Some(m) = SEMICOLON.find_at(input, cursor) {
+            if m.start() == cursor {
+                output.push(Token::LineEnd {});
+                cursor = m.end();
+                continue;
+            }
+        }
+
+        if let Some(m) = LOPERATOR.find_at(input, cursor) {
+            if m.start() == cursor {
+                output.push(Token::UnaryLeftOperator {
+                    operator: UnaryLeftOperator::NOTNULL,
+                });
+                cursor = m.end();
+                continue;
+            }
+        }
+
+        if let Some(m) = SPACING.find_at(input, cursor) {
+            if m.start() == cursor {
+                cursor = m.end();
+                continue;
+            }
+        }
+
+        panic!("Invalid character at position {}", cursor);
+    }
+
+    return cursor;
 }
